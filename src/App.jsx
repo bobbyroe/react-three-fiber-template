b474c7615240d322d84149ffd553204be8c73dc2
Bobby Roe <bobby.roe@unifyconsulting.com>
2024-11-10 12:29:16 -0800

    starter template for basic R3F projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import getLayer from "./getLayer";
+
+const bgSprites = getLayer({ 
+  numSprites: 8, 
+  radius: 10, 
+  z: -10.5, 
+  size: 24, 
+  opacity: 0.2, 
+  path: "./rad-grad.png" 
+})
+
+function IcoSphere() {
+  const icoRef = React.useRef();
+
+  useFrame(() => {
+    icoRef.current.rotation.x += 0.01;
+    icoRef.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={icoRef}>
+      <icosahedronGeometry />
+      <meshStandardMaterial color={0xffff00} />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
+    <Canvas>
+      <IcoSphere />
+      <hemisphereLight args={[0xffffff, 0x000000, 1.0]} />
+      <primitive object={bgSprites} />
+      <OrbitControls />
     </Canvas>
   );
 }
